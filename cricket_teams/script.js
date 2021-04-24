6dcd587d6756eadb9ec52e34a8cf01e12d160a02
atharva-01 <atharvagado4902@gamil.com>
2021-04-24 07:29:00 +0530

    initial commit

---
+++ cricket_teams/script.js
@@ -0,0 +1,9 @@
+function redirect(){
+    let teamName = document.getElementById('team_name').value;
+    switch(teamName){
+        case 'INDIA':
+            location.href = "indian_team.html";
+            break;
+        
+    }
+}
